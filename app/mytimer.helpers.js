7e868b4d91cf663122636a8b5ad7f1ad20c627f1
Kasia <ziomecka78@gmail.com>
2018-02-22 21:12:40 +0100

    removed helper hOP (has own property)

--- app/mytimer.helpers.js
+++ app/mytimer.helpers.js
@@ -5,16 +5,11 @@ function isPositiveInteger(value) {
   return (Number.isInteger(value) && value >= 0);
 }
 
-function hOP(obj, property) {
-  return {}.hasOwnProperty.call(obj, property);
-}
-
 function isObject(obj) {
   return Object(obj) === obj;
 }
 
 export default {
   isPositiveInteger: isPositiveInteger,
-  hOP: hOP,
   isObject: isObject
 };
