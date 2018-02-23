73cec55ef03e0fc085647733b4903a19fb50a923
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-02-23 08:08:06 +0100

    change in scenarios object: invalid level added

--- spec/scenarios/timer.scenarios.initialise.js
+++ spec/scenarios/timer.scenarios.initialise.js
@@ -22,138 +22,140 @@ module.exports = {
       }
     }
   },
-  "invalid count units": {
-    steps: {
-      session: {
-        value: 17,
-        units: "minutes"
+  invalid: {
+    "invalid count units": {
+      steps: {
+        session: {
+          value: 17,
+          units: "minutes"
+        },
+        interval: {
+          value: 21,
+          units: "milliseconds"
+        }
       },
-      interval: {
-        value: 21,
-        units: "milliseconds"
+      countUnits: ["invalid", "minutes", "seconds", "milliseconds"]
+    },
+    "invalid units": {
+      "Session": {
+        steps: {
+          session: {
+            value: 17,
+            units: "invalid"
+          },
+          interval: {
+            value: 21,
+            units: "milliseconds"
+          }
+        },
+        countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+      },
+      "Interval": {
+        steps: {
+          session: {
+            value: 17,
+            units: "milliseconds"
+          },
+          interval: {
+            value: 21,
+            units: "invalid"
+          }
+        },
+        countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+      },
+    },
+    "invalid values": {
+      "Session": {
+        steps: {
+          session: {
+            value: "17",
+            units: "hours"
+          },
+          interval: {
+            value: 21,
+            units: "milliseconds"
+          }
+        },
+        countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+      },
+      "Interval": {
+        steps: {
+          session: {
+            value: 17,
+            units: "milliseconds"
+          },
+          interval: {
+            value: "abc",
+            units: "hours"
+          }
+        },
+        countUnits: ["hours", "minutes", "seconds", "milliseconds"]
       }
     },
-    countUnits: ["invalid", "minutes", "seconds", "milliseconds"]
-  },
-  "invalid units": {
-    "Session": {
+    "lack of units": {
+      "Session": {
+        steps: {
+          session: {
+            value: 17
+          },
+          interval: {
+            value: 21,
+            units: "milliseconds"
+          }
+        },
+        countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+      },
+      "Interval": {
+        steps: {
+          session: {
+            value: 17,
+            units: "seconds"
+          },
+          interval: {
+            value: 21,
+          }
+        },
+        countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+      },
+    },
+    "lack of value": {
+      "Session": {
+        steps: {
+          session: {
+            units: "milliseconds"
+          },
+          interval: {
+            value: 17,
+            units: "milliseconds"
+          }
+        },
+        countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+      },
+      "Interval": {
+        steps: {
+          session: {
+            value: 17,
+            units: "seconds"
+          },
+          interval: {
+            units: "milliseconds"
+          }
+        },
+        countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+      },
+    },
+    "only some countUnits": {
       steps: {
         session: {
           value: 17,
-          units: "invalid"
+          units: "seconds"
         },
         interval: {
           value: 21,
           units: "milliseconds"
         }
       },
-      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
-    },
-    "Interval": {
-      steps: {
-        session: {
-          value: 17,
-          units: "milliseconds"
-        },
-        interval: {
-          value: 21,
-          units: "invalid"
-        }
-      },
-      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
-    },
-  },
-  "invalid values": {
-    "Session": {
-      steps: {
-        session: {
-          value: "17",
-          units: "hours"
-        },
-        interval: {
-          value: 21,
-          units: "milliseconds"
-        }
-      },
-      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
-    },
-    "Interval": {
-      steps: {
-        session: {
-          value: 17,
-          units: "milliseconds"
-        },
-        interval: {
-          value: "abc",
-          units: "hours"
-        }
-      },
-      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+      countUnits: ["hours", "seconds", "minutes"]
     }
-  },
-  "lack of units": {
-    "Session": {
-      steps: {
-        session: {
-          value: 17
-        },
-        interval: {
-          value: 21,
-          units: "milliseconds"
-        }
-      },
-      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
-    },
-    "Interval": {
-      steps: {
-        session: {
-          value: 17,
-          units: "seconds"
-        },
-        interval: {
-          value: 21,
-        }
-      },
-      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
-    },
-  },
-  "lack of value": {
-    "Session": {
-      steps: {
-        session: {
-          units: "milliseconds"
-        },
-        interval: {
-          value: 17,
-          units: "milliseconds"
-        }
-      },
-      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
-    },
-    "Interval": {
-      steps: {
-        session: {
-          value: 17,
-          units: "seconds"
-        },
-        interval: {
-          units: "milliseconds"
-        }
-      },
-      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
-    },
-  },
-  "only some countUnits": {
-    steps: {
-      session: {
-        value: 17,
-        units: "seconds"
-      },
-      interval: {
-        value: 21,
-        units: "milliseconds"
-      }
-    },
-    countUnits: ["hours", "seconds", "minutes"]
   }
 };
