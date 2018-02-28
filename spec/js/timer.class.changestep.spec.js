7a861ef7eaba99a0b5c9f1dfbbe8692d30c9ce4e
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-02-28 15:41:39 +0100

    added test for decresing step more than the step's length

--- spec/js/timer.class.changestep.spec.js
+++ spec/js/timer.class.changestep.spec.js
@@ -1,12 +1,4 @@
 /* jshint esversion: 6 */
-
-// TODO split between files
-// TODO test creating and removing methods
-// TODO test real counting
-// TODO return correct values when stepChanged
-// TODO test passing negative values - error expected
-// TODO what if direction changes when ccounting?
-
 import Defaults from "../../app/mytimer.defaults";
 import Timer from "../../app/mytimer.class";
 import scenariosChangeStep from "../scenarios/timer.scenarios.changestep";
@@ -14,7 +6,7 @@ import messages from "../../app/mytimer.messages";
 
 let defs = new Defaults();
 
-describe("Timer: ", () => {
+describe("When timer is stopped and step is changed: ", () => {
 
   scenariosChangeStep.sessionStep.forEach((scenario) => {
     let timer = new Timer();
@@ -25,6 +17,7 @@ describe("Timer: ", () => {
 				*/
 		let scenarioI = scenario.increment;
     let scenarioS = scenario.sign;
+    let step = scenario.step;
 		let increment = (scenarioI === 1 || scenarioI === 0)? scenarioI : 0;
     let sign = (scenarioS === 1 || scenarioS === -1)? scenarioS : 1;
     let expectedValue = timer._this.convert(scenario) * sign + timer.session * increment;
@@ -36,16 +29,53 @@ describe("Timer: ", () => {
 							(increment === 1)? "increments" :
 							(increment === -1)? "decrements" : "wrong scenario";
 
-		it(`${name} session ${word} ${change}`, () => {
+    it(`the ${step} is changed ${word} ${change}, then the timer returns correct ${step}`, () => {
       timer.changeStep(scenario);
-      expect(timer.session).toBe(expectedValue > 0? expectedValue : 0);
+      expect(timer[step]).toBe(expectedValue > 0? expectedValue : 0);
     });
   });
+});
 
+describe("When timer's changeStep method get's invalid arguments: ", () => {
   scenariosChangeStep.invalidStep.forEach((scenario) => {
     let timer = new Timer();
-    it(`throws error if step is invalid`, () => {
+    it(`invalid step's name: the timer throws error.`, () => {
       expect(() => timer.changeStep(scenario)).toThrowError(messages.stepNotChanged);
     });
   });
+});
+
+describe("When timer is stopped and the step is decreased several times and the accumulated change is higher than the step's length: ", () => {
+    let scenario = {
+      step: "session",
+      value: 1,
+      units: "minutes",
+      increment: 1,
+      sign: -1
+    };
+    let timerOptions = {
+      steps: {
+        session: {
+          value: 5,
+          units: "minutes"
+        },
+        interval: {
+          value: 1,
+          units: "milliseconds"
+        }
+      },
+      countUnits: ["hours", "minutes", "seconds", "milliseconds"]
+    };
+    let timer = new Timer(timerOptions);
+    let change = timer._this.convert(scenario);
+    let step = scenario.step;
+    let times = Math.ceil(timer._this.convert(timerOptions.steps[step]) / change) + 1;
+    console.log(times);
+    for (let i = 0; i < times; i++) {
+      timer.changeStep(scenario);
+    }
+    it(`the step's final length is zero.`, () => {
+      expect(timer[step]).toBe(0);
+    });
+    console.log(timer[step]);
 });
